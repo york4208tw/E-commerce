c98dc49381e6c3708f2f2c0f177d696fa2e79ad6
york4208tw <72615318+york4208tw@users.noreply.github.com>
2023-03-10 16:16:47 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $('.favorite').click(function (e) { 
+        $( this ).toggleClass('fave-select');
+    });
+
+    $('.favorite').hover(function (e) { 
+        $( this ).animate({
+            width: "26px",
+            'background-size': "25px",
+            top: "17px",
+            right: "20px",
+        },200);
+    },function (e) {
+        $( this ).animate({
+            width: "20px",
+            'background-size': "20px",
+            top: "19px",
+            right: "22px",
+        },200);
+    });
+
+    $('.product-btn,.checkout-btn').hover(function (e) { 
+        $( this ).toggleClass(' animate__animated animate__pulse');
+    });
+    $('.navbar-r li,.footer-link li,.product-delete').hover(function (e) { 
+        $( this ).toggleClass(' animate__animated animate__heartBeat');
+    });
+    $('.logo,.mailbar-btn,.login-btn,.rememberme span').hover(function (e) { 
+        $( this ).toggleClass(' animate__animated animate__pulse');
+    });
+    $('.slide-bar .options a').hover(function (e) { 
+        $( this ).toggleClass('title-select');
+    });
+    $('.page-btns .options').hover(function (e) { 
+        $( this ).toggleClass('page-btn-select');
+    });
+    $('.page-arrow').hover(function (e) { 
+        $( this ).toggleClass('title-select');
+    });
+
+
+});
+
